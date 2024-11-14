35bad729146df73d5fbe05f8a15b409c5c9552eb
parkerw <william.parker@physics.ox.ac.uk>
2024-11-14 21:02:03 +0000

    Have wall time in submission script and run make osc grids over all cores

--- util/submitCondor.py
+++ util/submitCondor.py
@@ -1,7 +1,7 @@
-import sys
 import os
-import string
 import argparse
+import configparser
+import json
 
 def check_dir(dname):
     """Check if directory exists, create it if it doesn't"""
@@ -12,41 +12,58 @@ def check_dir(dname):
         os.stat(direc)
     except:
         os.makedirs(direc)
-        print "Made directory %s...." % dname
+        print ("Made directory %s...." % dname)
     return dname
 
-def pycondor_submit(job_batch, job_id, run_path, dataset_dir, dataset_name, fit_dir, dim, fit_config, cut_config, pdf_config, syst_config, env_file, sleep_time = 1, priority = 5):
+
+def pycondor_submit(job_name, exec_name, out_dir, run_dir, env_file, fit_config, rates_config, pdf_config, syst_config, osc_config, walltime, sleep_time = 1, priority = 5):
     '''
     submit a job to condor, write a sh file to source environment and execute command
     then write a submit file to be run by condor_submit
     '''
 
-    print job_id
+    print (job_name)
+    if exec_name == "make_osc_grids":
+        batch_name, index = job_name.split('_index_')
+    else:
+        index = ""
+        batch_name = job_name
 
     ### set a condor path to be called later
     
-    condor_path = "{0}/".format(fit_dir)
-    exec_path = run_path + "/bin/fit_dataset"
+    condor_path = "{0}/".format(out_dir)
+    exec_path = run_dir + "/bin/" + exec_name
 
     configs_path = os.path.abspath('{0}/cfg'.format(condor_path))
     check_dir(configs_path)
-    os.system("cp " + str(fit_config) + " " + str(configs_path) + "/" + os.path.basename(fit_config) )
-    os.system("cp " + str(pdf_config) + " " + str(configs_path) + "/" + os.path.basename(pdf_config) )
-    os.system("cp " + str(cut_config) + " " + str(configs_path) + "/" + os.path.basename(cut_config) )
-    os.system("cp " + str(syst_config) + " " + str(configs_path) + "/" + os.path.basename(syst_config) )
 
-
-    dataset_file = dataset_dir + dataset_name
+    if fit_config != "":
+        os.system("cp " + str(fit_config) + " " + str(configs_path) + "/" + os.path.basename(fit_config) )
+        fit_config = configs_path + "/" + fit_config.split("/")[-1]
+    if rates_config != "":
+        os.system("cp " + str(rates_config) + " " + str(configs_path) + "/" + os.path.basename(rates_config) )
+        rates_config = configs_path + "/" + rates_config.split("/")[-1]
+    if pdf_config != "":
+        os.system("cp " + str(pdf_config) + " " + str(configs_path) + "/" + os.path.basename(pdf_config) )
+        pdf_config = configs_path + "/" + pdf_config.split("/")[-1]
+    if syst_config != "":
+        os.system("cp " + str(syst_config) + " " + str(configs_path) + "/" + os.path.basename(syst_config) )
+        syst_config = configs_path + "/" + syst_config.split("/")[-1]
+    if osc_config != "":
+        os.system("cp " + str(osc_config) + " " + str(configs_path) + "/" + os.path.basename(osc_config) )
+        osc_config = configs_path + "/" + osc_config.split("/")[-1]
 
     other_commands = 'sleep $[($RANDOM%' + str(sleep_time+1) + ')+1]s'
 
     out_macro_text = "#!/usr/bin/sh  \n" + \
-                     "source " + str(env_file) + "\n" + \
-                     "cd " + str(run_path) + "\n" + \
+                     "source " + env_file + "\n" + \
+                     "source " + run_dir + "/env.sh " + "\n" + \
+                     "echo $HOME \n" + \
+                     "cd " + str(run_dir) + "\n" + \
                      str(other_commands) + "\n" + \
-                     str(exec_path) + " " + str(configs_path) + "/" + os.path.basename(fit_config) + " " + str(configs_path) + "/" + os.path.basename(pdf_config) + " " + str(configs_path) + "/" + os.path.basename(cut_config) + " " + str(configs_path) + "/" + os.path.basename(syst_config) + " " + str(dataset_file) + " " + str(dim) + " " + str(fit_dir) + "\n"
+                     str(exec_path) + " " + str(fit_config) + " " + str(rates_config) + " " + str(pdf_config) + " " + str(syst_config) + " " + str(osc_config) + " " + index
 
-    sh_filepath = "{0}/sh/".format(condor_path) + str(job_id).replace("/", "") + '.sh'
+    sh_filepath = "{0}/sh/".format(condor_path) + str(job_name).replace("/", "") + '.sh'
     if not os.path.exists(os.path.dirname(sh_filepath)):
         os.makedirs(os.path.dirname(sh_filepath))
     sh_file = open(sh_filepath, "w")
@@ -63,18 +80,19 @@ def pycondor_submit(job_batch, job_id, run_path, dataset_dir, dataset_name, fit_
     universe = "vanilla"
     notification = "never"
     n_rep = 1
-    getenv = "False"
+    getenv = "True"
 
-    submit_filepath = os.path.join(submit_path, job_id)
+    submit_filepath = os.path.join(submit_path, job_name)
     submit_filepath += ".submit"
     out_submit_text = "executable              = " + str(sh_filepath) + "\n" + \
-                     "universe                = " + str(universe) + "\n" + \
-                     "output                  = " + str(output_path) + "/" + str(job_id) + ".output\n" + \
-                     "error                   = " + str(error_path) + "/" + str(job_id) + ".error\n" + \
-                     "log                     = " + str(log_path) + "/" + str(job_id) + ".log\n" + \
-                     "notification            = " + str(notification) + "\n" + \
-                     "priority                = " + str(priority) + "\n" + \
-                     "getenv                  = " + str(getenv) + "\n" + \
+                     "universe                 = " + str(universe) + "\n" + \
+                     "output                   = " + str(output_path) + "/" + str(job_name) + ".output\n" + \
+                     "error                    = " + str(error_path) + "/" + str(job_name) + ".error\n" + \
+                     "log                      = " + str(log_path) + "/" + str(job_name) + ".log\n" + \
+                     "notification             = " + str(notification) + "\n" + \
+                     "priority                 = " + str(priority) + "\n" + \
+                     "getenv                   = " + str(getenv) + "\n" + \
+                     "allowed_execute_duration = " + str(walltime) + " \n" + \
                      "queue "+str(n_rep)+"\n"
 
     ## check and create output path
@@ -84,59 +102,89 @@ def pycondor_submit(job_batch, job_id, run_path, dataset_dir, dataset_name, fit_
     out_submit_file.write(out_submit_text)
     out_submit_file.close()
 
-    command = 'condor_submit -batch-name \"' + job_batch+'\" ' + submit_filepath
-    print "executing job: " + command
+
+    command = 'condor_submit -batch-name \"' + batch_name +'\" ' + submit_filepath
+    print ("executing job: " + command)
     os.system(command)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser("Launch a load of fits")
-    parser.add_argument('dataset_dir', type=str, help='Dataset directory. Top level directory where ntuples e_r_class_ntups directories have been written in. Intended structure is to write all youur datasets made from these ntuples into this directory, as well as any fits to those datasets. ')
-    parser.add_argument('dataset_name', type=str, help='Dataset filename. Should be an h5 file within dataset_dir (with extension)')
-    parser.add_argument('fit_dir', type=str, help='Fit directory name. This will be made within dataset directory.')
-    parser.add_argument('-d', "--dims", type=str, help='How any dimensions?')
-    parser.add_argument('-f', "--fit_cfg", type=str, help='Fit config path')
-    parser.add_argument('-c', "--cut_cfg", type=str, help='Cut config path')
-    parser.add_argument('-p', "--pdf_cfg", type=str, help='PDF config path')
-    parser.add_argument('-s', "--syst_cfg", type=str, help='Systematic config path')
-    parser.add_argument('-e', "--env_file", type=str, help='Environment file to source. Absolute path.')
-    parser.add_argument("-n", "--no_sims", type=int,
-                       default=10,
-                       help="How many identical fits would you like to run?")
+    parser = argparse.ArgumentParser("Submit jobs to condor")
+    parser.add_argument('exe', type=str, help='executable')
+    parser.add_argument('out_dir', type=str, help='output directory')
+    parser.add_argument('-r', "--run_dir", type=str, default=".", help='directory to run executable from')
+    parser.add_argument('-e', "--env_file", type=str, default="", help='environment variable file to source')
+    parser.add_argument('-f', "--fit_cfg", type=str, default="", help='fit config path')
+    parser.add_argument('-i', "--rates_cfg", type=str, default="", help='event rates config path')
+    parser.add_argument('-p', "--pdf_cfg", type=str, default="", help='pdf config path')
+    parser.add_argument('-s', "--syst_cfg", type=str, default="", help='syst config path')
+    parser.add_argument('-o', "--osc_cfg", type=str, default="", help='osc grid config path')
+    parser.add_argument("-n", "--num_jobs", type=int, default=1, help="how many identical jobs would you like to run?")
+    parser.add_argument("-w", "--wall_time", type=int, default=86400, help="what's the maximum runtime (in seconds, default 1 day)?")
     args = parser.parse_args()
 
     ## check if output and condor directories exist, create if they don't
-    bb_dir = os.getenv('BB_DIR')
-    data_dir = os.getenv('DATA_DIR')
-    dataset_dir = data_dir + args.dataset_dir
-    dataset_dir = check_dir(dataset_dir)
-    dataset_name = args.dataset_name
-    base_name = args.fit_dir
-    over_dir = check_dir(dataset_dir+"/"+base_name) 
-    fit_config = bb_dir + "../results/" + args.fit_cfg
-    cut_config = bb_dir + "../cuts/" + args.cut_cfg
-    pdf_config = bb_dir + "../pdfs/" + args.pdf_cfg
-    syst_config = bb_dir + "../systs/" + args.syst_cfg
+    exec_name = args.exe
+    out_dir = check_dir(args.out_dir)
+    base_name = out_dir.split("/")[-2] #os.path.basename(out_dir)
+    run_dir = args.run_dir
     env_file = args.env_file
+    fit_config = args.fit_cfg
+    rates_config = args.rates_cfg
+    pdf_config = args.pdf_cfg
+    syst_config = args.syst_cfg
+    osc_config = args.osc_cfg
+    walltime = args.wall_time
+    if fit_config != "":
+        fit_config = run_dir + "/" + args.fit_cfg
+    if rates_config != "":
+        rates_config = run_dir + "/" + args.rates_cfg
+    if pdf_config != "":
+        pdf_config = run_dir + "/" + args.pdf_cfg
+    if syst_config != "":
+        syst_config = run_dir + "/" + args.syst_cfg
+    if osc_config != "":
+        osc_config = run_dir + "/" + args.osc_cfg
 
-    dims = (args.dims)
+    if exec_name == "make_osc_grids":
+        # Get reactors json from oscgrid
+        config = configparser.ConfigParser()
+        config.read(osc_config)
 
-    for i in range(args.no_sims):
+        # Access the reactors json filepath under the 'summary' section
+        reactorsjson_path = config.get('summary', 'reactorsjson')
 
-        job_name = base_name + "_{0}".format(i)
-        fit_dir = over_dir + "/" + job_name
-        fit_dir = check_dir(fit_dir)
-        dims = (args.dims)
-        
-        condor_directory = "{0}/condor".format(bb_dir)
-        
-        log_dir = check_dir("{0}/log/".format(fit_dir))
-        error_dir = check_dir("{0}/error/".format(fit_dir))
-        sh_dir = check_dir("{0}/sh/".format(fit_dir))
-        submit_dir = check_dir("{0}/submit/".format(fit_dir))
-        output_dir = check_dir("{0}/output/".format(fit_dir))
+        # Load the JSON file
+        with open(reactorsjson_path) as f:
+            data = json.load(f)
 
-        job_id = "{0}_{1}".format(base_name,i)
-        batch_id = "{0}".format(args.fit_dir)
-        pycondor_submit(batch_id, job_id, bb_dir, dataset_dir, dataset_name, fit_dir, dims, fit_config, cut_config, pdf_config, syst_config, env_file, sleep_time = 1, priority = 5)
+        # Loop over each key-value pair
+        for index, values in data.items():
+            # Convert the key to an integer
+            int_index = int(index)
+            # Get the string and double values from the list
+            reac_name, distance_val = values
+
+            job_name = base_name + "_index_{0}".format(int_index)
+
+            log_dir = check_dir("{0}/log/".format(out_dir))
+            error_dir = check_dir("{0}/error/".format(out_dir))
+            sh_dir = check_dir("{0}/sh/".format(out_dir))
+            submit_dir = check_dir("{0}/submit/".format(out_dir))
+            output_dir = check_dir("{0}/output/".format(out_dir))
+
+            pycondor_submit(job_name, exec_name, out_dir, run_dir, env_file, fit_config, rates_config, pdf_config, syst_config, osc_config, walltime, sleep_time = 1, priority = 5)
+
+    else:
+        for i in range(args.num_jobs):
+
+            job_name = base_name + "_{0}".format(i)
+
+            log_dir = check_dir("{0}/log/".format(out_dir))
+            error_dir = check_dir("{0}/error/".format(out_dir))
+            sh_dir = check_dir("{0}/sh/".format(out_dir))
+            submit_dir = check_dir("{0}/submit/".format(out_dir))
+            output_dir = check_dir("{0}/output/".format(out_dir))
+
+            pycondor_submit(job_name, exec_name, out_dir, run_dir, env_file, fit_config, rates_config, pdf_config, syst_config, osc_config, walltime, sleep_time = 1, priority = 5)
