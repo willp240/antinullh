5788f9c3c80b8ad9fd929d04831829046c37ff58
parkerw <william.parker@physics.ox.ac.uk>
2025-01-09 18:53:24 +0000

    fix how we make reactor dist

--- exec/grid_fit.cc
+++ exec/grid_fit.cc
@@ -179,7 +179,7 @@ void grid_fit(const std::string &fitConfigFile_,
     if (it->first == "reactor_nubar")
     {
       // Build the distribution with oscillation parameters at their nominal values
-      DistBuilder::BuildOscillatedDist(it->first, num_dimensions, pdfConfig, dataSet, deltam21, theta12, indexDistance);
+      dist = DistBuilder::BuildOscillatedDist(it->first, num_dimensions, pdfConfig, dataSet, deltam21, theta12, indexDistance);
     }
     else
     {
